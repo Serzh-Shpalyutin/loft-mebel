ec9a8fc857107c00d34eb205f530abbb5b4222d3
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2024-03-21 15:42:48 +0300

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <header class="header">App header</header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <AppHeader />
+
+  <main class="main">
+    <slot />
+  </main>
+
+  <AppFooter />
+</template>
