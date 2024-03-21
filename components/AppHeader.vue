4c4494fac708d212be5e4c647384a4b7d029c38c
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2024-03-21 17:53:42 +0300

    add header top

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,39 @@
 <template>
-  <header class="header">App header</header>
+  <header class="header">
+    <div class="header__top">
+      <div class="container header__top-container">
+        <HeaderTopNav />
+
+        <div class="header__top-tel">
+          <img src="@/assets/img/svg/phone.svg" alt="">
+          <a href="tel:89648999119">8 (964) 89 99 119</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="container header__middle"></div>
+
+    <div class="container header__bottom"></div>
+  </header>
 </template>
+
+<style lang="scss" scoped>
+.header {
+  &__top {
+    background-color: #313131;
+    padding: 11px 0;
+  }
+
+  &__top-tel,
+  &__top-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__top-tel {
+    img {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
---
+++ components/HeaderTopNav.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+const HeaderTopNav = [
+  {
+    name: 'Главная',
+    link: '/'
+  },
+  {
+    name: 'О нас',
+    link: '/about'
+  },
+  {
+    name: 'Контакты',
+    link: '/contacts'
+  }
+];
+</script>
+
+<template>
+  <nav v-if="HeaderTopNav.length" class="header-top-nav">
+    <ul class="header-top-nav__list list-reset">
+      <li v-for="item in HeaderTopNav" :key="item.name" class="header-top-nav__item">
+        <NuxtLink class="header-top-nav__link" :to="item.link">{{ item.name }}</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.header-top-nav {
+  &__list {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+  }
+}
+</style>
